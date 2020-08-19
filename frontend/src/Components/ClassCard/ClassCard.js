9122cec4778a68d3a5ff48f15863b9fed088fe45
Babak <babakjahangiri@gmail.com>
2020-08-19 23:41:10 +0100

    add conditions & Child prop

--- frontend/src/Components/ClassCard/ClassCard.js
+++ frontend/src/Components/ClassCard/ClassCard.js
@@ -3,10 +3,11 @@ import { MonthNames } from "../../Utils/MonthNames";
 import { Link } from "react-router-dom";
 import dayjs from "dayjs";
 import "./ClassCard.scss";
+import NewBookingForm from "../NewBookingForm/NewBookingForm";
 
-const ClassCard = ({ Title, Classes }) => {
+const ClassCard = ({ Title, Child, Classes }) => {
   return (
-    <div>
+    <React.Fragment>
       {Classes.data && Classes.data.length > 0
         ? [
             Classes.data
@@ -17,45 +18,54 @@ const ClassCard = ({ Title, Classes }) => {
           ].map((Class, index) => (
             <div className="ClassCard_Body" key={index}>
               <div className="ClassCard_Main">
-                {Title != "" && <p>{Title}</p>}
-                <div className="ClassCard_Container">
-                  <div className="ClassCard_Date">
-                    <p>{new Date(Class.date).getDate().toString()}</p>
-                    <p>
-                      {MonthNames[new Date(Class.date).getMonth().toString()]}
-                    </p>
-                  </div>
-                  <div className="ClassCard_info">
-                    <div className="ClassCard_top">
-                      <div>
-                        <p className="ClassCard_ClassName">{Class.className}</p>
-                        <p>{Class.scheduleType}</p>
-                        <p>{Class.time}</p>
+                {Title !== "" && <p>{Title}</p>}
+                <div className="ClassCard_Border">
+                  <div className="ClassCard_Container">
+                    <div className="ClassCard_Date">
+                      <p>{new Date(Class.date).getDate().toString()}</p>
+                      <p>
+                        {MonthNames[new Date(Class.date).getMonth().toString()]}
+                      </p>
+                    </div>
+                    <div className="ClassCard_info">
+                      <div className="ClassCard_top">
+                        <div>
+                          <p className="ClassCard_ClassName">
+                            {Class.className}
+                          </p>
+                          <p>{Class.scheduleType}</p>
+                          <p>{Class.time}</p>
+                        </div>
+                        <div>
+                          <a
+                            href={Class.syllabusUrl}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            <div className="ClassCard_Syllabus">
+                              <p>Syllabus</p>
+                              <i className="fas fa-book-open"></i>
+                            </div>
+                          </a>
+                        </div>
                       </div>
-                      <div>
-                        <a
-                          href={Class.syllabusUrl}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <div className="ClassCard_Syllabus">
-                            <p>Syllabus</p>
-                            <i className="fas fa-book-open"></i>
-                          </div>
-                        </a>
+                      <div className="ClassCard_bottom">
+                        <p>0 voluntieers signed up</p>
+                        {Child == null && <Link to="/newbooking/">Attend</Link>}
                       </div>
                     </div>
-                    <div className="ClassCard_bottom">
-                      <p>0 voluntieers signed up</p>
-                      <Link to="/newbooking/">Attend</Link>
-                    </div>
                   </div>
+                  {Child != null && <hr className="booking-separator"></hr>}
+                  {Child === "newbooking" && <NewBookingForm></NewBookingForm>}
+                  {Child === "volunteerslist" && (
+                    <h3>list of volunteers appear here</h3>
+                  )}
                 </div>
               </div>
             </div>
           ))
         : null}
-    </div>
+    </React.Fragment>
   );
 };
 
