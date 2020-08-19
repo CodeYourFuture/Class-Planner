cc35903a9c0d79600d0e1afa2208de54ff17fa51
byohannes <byohannes44@gmail.com>
2020-08-19 15:40:51 +0100

    created booking validation on request data

---
+++ backend/validation/booking.js
@@ -0,0 +1,24 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateBookingInput(data) {
+  let errors = {};
+
+  data.fullName = !isEmpty(data.fullName) ? data.fullName : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.roleName = !isEmpty(data.roleName) ? data.roleName : "";
+  
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+  if (Validator.isEmpty(data.roleName)) {
+    errors.roleName = "Role field is required";
+  }
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
+  if (Validator.isEmpty(data.fullName)) {
+    errors.fullName = "Full name field is required";
+  }
+  return { errors, isValid: isEmpty(errors) };
+};
