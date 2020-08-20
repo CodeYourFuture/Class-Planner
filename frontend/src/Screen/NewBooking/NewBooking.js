22fbc5f8338143d50cd86128379f2e6ea7a0e4ad
Babak <babakjahangiri@gmail.com>
2020-08-20 13:51:28 +0100

    create newbooking form screen

---
+++ frontend/src/Screen/NewBooking/NewBooking.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { Get_Classes } from "../../Redux/Actions";
+import Header from "../../Components/Header/Header";
+import ClassCard from "../../Components/ClassCard";
+import Footer from "../../Components/Footer/Footer";
+
+const mapStateToProps = (state) => {
+  return { classes: state.ClassReducer.classes };
+};
+
+export default connect(mapStateToProps, { Get_Classes })(
+  ({ classes, Get_Classes, match }) => {
+    const { params } = match;
+
+    useEffect(() => {
+      Get_Classes();
+    }, [Get_Classes]);
+
+    return (
+      <React.Fragment>
+        <Header parm={params} NavState="new booking" />
+        <ClassCard Classes={classes} Child="newbooking" />
+        <Footer />
+      </React.Fragment>
+    );
+  }
+);
