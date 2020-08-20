0d1cf55637dbf73afd660fe76cf146465c4eb2b7
Babak <babakjahangiri@gmail.com>
2020-08-20 13:31:11 +0100

    add newbooking to Route

--- frontend/src/Routes/index.js
+++ frontend/src/Routes/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Home from "../Screen/Home/Home"
+import Home from "../Screen/Home/Home";
 import UpcomingClass from "../Screen/UpcomingClass/UpcomingClass";
 import { BrowserRouter as Switch, Route } from "react-router-dom";
 
@@ -9,6 +9,8 @@ export default () => {
       <Route exact path="/" component={Home} />
       <Route exact path="/upcomingclass/:user" component={UpcomingClass} />
       <Route exact path="/upcomingclass/" component={UpcomingClass} />
+      <Route exact path="/newbooking/:user" component={NewBooking} />
+      <Route exact path="/newbooking/" component={NewBooking} />
     </Switch>
   );
 };
