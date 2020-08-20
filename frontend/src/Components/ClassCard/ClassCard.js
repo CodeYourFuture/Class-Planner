f49bce6bdfb65cf98bd2d2da98c4f84f1111cfe6
Babak <babakjahangiri@gmail.com>
2020-08-20 12:27:30 +0100

    update & fix css

--- frontend/src/Components/ClassCard/ClassCard.js
+++ frontend/src/Components/ClassCard/ClassCard.js
@@ -14,37 +14,41 @@ const ClassCard = ({ Classes }) => {
                 (Class) => Class.status && dayjs(Class.date) > dayjs(new Date())
               ),
           ].map((Class, index) => (
-            <div className="ClassCard_Body" key={index}>
-              <div className="ClassCard_Main">
+            <div className="classcard-body" key={index}>
+              <div className="classcard-main">
                 <p>Next upcoming Class:</p>
-                <div className="ClassCard_Class">
-                  <div className="ClassCard_Date">
-                    <div>
+                <div className="classcard-border">
+                  <div className="classcard-container">
+                    <div className="classcard-date">
                       <p>{new Date(Class.date).getDate().toString()}</p>
                       <p>
                         {MonthNames[new Date(Class.date).getMonth().toString()]}
                       </p>
                     </div>
-                  </div>
-                  <div className="ClassCard_info">
-                    <div className="ClassCard_info_1">
-                      <div>
-                        <p className="ClassCard_ClassName">{Class.className}</p>
-                        <p>{Class.scheduleType}</p>
-                        <p>{Class.time}</p>
+                    <div className="classcard-info">
+                      <div className="classcard-top">
+                        <div>
+                          <p className="classcard-title">{Class.className}</p>
+                          <p>{Class.scheduleType}</p>
+                          <p>{Class.time}</p>
+                        </div>
+                        <div>
+                          <a
+                            href={Class.syllabusUrl}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            <div className="classcard-syllabus">
+                              <p>Syllabus</p>
+                              <i className="fas fa-book-open"></i>
+                            </div>
+                          </a>
+                        </div>
                       </div>
-                      <div>
-                        <a href={Class.syllabusUrl} target="_blank" rel="noopener noreferrer">
-                          <div className="ClassCard_Syllabus">
-                            <p className="ClassCard_Syllabus_P">Syllabus</p>
-                            <i className="fas fa-book-open"></i>
-                          </div>
-                        </a>
+                      <div className="classcard-bottom">
+                        <p>0 voluntieers signed up</p>
+                        <a>Book</a>
                       </div>
-                    </div>
-                    <div className="ClassCard_info_2">
-                      <p>0 voluntieers signed up</p>
-                      <p>Book</p>
                     </div>
                   </div>
                 </div>
