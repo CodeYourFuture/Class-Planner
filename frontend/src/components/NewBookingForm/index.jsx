1733e3373e78d2fe968847f0f5ceb09777c103ba
Babak <babakjahangiri@gmail.com>
2020-08-21 10:15:39 +0100

    add styles to form

--- frontend/src/components/NewBookingForm/index.jsx
+++ frontend/src/components/NewBookingForm/index.jsx
@@ -1,34 +1,45 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./NewBookingForm.scss";
 
 const NewBookingForm = () => {
   return (
     <div className="newbooking-container">
-      <form method="post">
-        <label for="fullName">Full name:</label>
-        <input
-          id="fullName"
-          type="text"
-          name="fullName"
-          className="form-control margin-bottom-20"
-        />
-        <label for="email">Email:</label>
-        <input
-          id="email"
-          type="email"
-          name="email"
-          className="form-control margin-bottom-20"
-        />
-        <label for="role">Role:</label>
-        <select id="role">
-          <option value="0">Select your preferred role</option>
-          <option value="1">Coordinator</option>
-          <option value="2">Lead Teacher</option>
-          <option value="3">Assistant Lead Teacher</option>
-          <option value="4">Teaching Assistant</option>
-          <option value="5">Personal Development Rep</option>
-        </select>
+      <form method="post" className="newbooking-form">
+        <div className="form-group">
+          <label for="fullName">Full name:</label>
+          <input
+            id="fullName"
+            type="text"
+            name="fullName"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label for="email">Email:</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label for="role">Role:</label>
+          <select id="role" className="form-control">
+            <option value="0">Select your preferred role</option>
+            <option value="1">Coordinator</option>
+            <option value="2">Lead Teacher</option>
+            <option value="3">Assistant Lead Teacher</option>
+            <option value="4">Teaching Assistant</option>
+            <option value="5">Personal Development Rep</option>
+          </select>
+        </div>
+
+        <div className="form-group">
+          <input type="submit"></input>
+        </div>
       </form>
     </div>
   );
