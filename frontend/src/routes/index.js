9e7757501d53ec33363016a90d14e899d0ce3463
AKaramifar <a.karamifar@gmail.com>
2020-08-25 00:21:37 +0100

    CourseCalendar-Rout

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,6 +3,7 @@ import Home from "../screen/Home/Home.jsx";
 import UpcomingClass from "../screen/UpcomingClass/UpcomingClass.jsx";
 import NewClass from "../screen/NewClass/NewClass.jsx";
 import NewBooking from "../screen/NewBooking/NewBooking.jsx";
+import CourseCalendar from "../screen/CourseCalendar/CourseCalendar.jsx";
 
 import { BrowserRouter as Switch, Route } from "react-router-dom";
 
@@ -10,9 +11,10 @@ export default () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/coursecalendar/:user" component={CourseCalendar} />
       <Route exact path="/upcomingclass/:user" component={UpcomingClass} />
       <Route exact path="/newclass/:user" component={NewClass} />
-      <Route exact path="/newbooking/:user" component={NewBooking} />
+      <Route exact path="/newbooking/:user" component={NewBooking} />      
     </Switch>
   );
 };
