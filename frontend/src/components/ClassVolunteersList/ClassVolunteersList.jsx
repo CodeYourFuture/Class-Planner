7d415a7823f63f701c58456e3b47629933761c80
Babak <babakjahangiri@gmail.com>
2020-08-26 13:29:49 +0100

    init html source

--- frontend/src/components/ClassVolunteersList/ClassVolunteersList.jsx
+++ frontend/src/components/ClassVolunteersList/ClassVolunteersList.jsx
@@ -1,10 +1,26 @@
-import React from "react";
-import "./ClassVolunteersList.scss";
-
 const ClassVolunteersList = ({ bookings }) => {
   return (
     <div className="classvolunteerslist-container">
-      <h1>{bookings[0].fullName}</h1>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">FullName</th>
+            <th scope="col">Role</th>
+            <th scope="col">Email</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{bookings[0].fullName}</td>
+            <td>Coordinator</td>
+            <td>@mdo</td>
+            <td>
+              <button>Cancel</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
