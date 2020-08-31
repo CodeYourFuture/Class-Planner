8dda549324838a88b523fb08f965248b746102ef
byohannes <byohannes44@gmail.com>
2020-08-31 02:45:45 +0100

    updated index file of redux action forupdate action

--- frontend/src/redux/actions/index.js
+++ frontend/src/redux/actions/index.js
@@ -1,4 +1,10 @@
-export { Get_UpcomingClass, Get_Classes, createClass, set_CurrentClass } from "./ClassAction";
+export {
+  Get_UpcomingClass,
+  Get_Classes,
+  createClass,
+  updateClass,
+  set_CurrentClass,
+} from "./ClassAction";
 export { createBooking, Get_BookingByClassId } from "./BookingAction";
 export { Get_Courses } from "./CourseAction";
 export { Send_PageData } from "./PageAction";
