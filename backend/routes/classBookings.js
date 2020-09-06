beb5f5b4a681d5af9ccb5a733592ca88dffc6a2b
byohannes <byohannes44@gmail.com>
2020-09-06 23:31:14 +0100

    deleted backend backend/routes/classBookings file

--- backend/routes/classBookings.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getBookingsByClassId } = require("../controllers/bookings");
-
-router.route("/:classId").get(getBookingsByClassId);
-
-module.exports = router;
