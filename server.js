d8ff4a882f802dfe5e3dd172bda1756937c90f27
byohannes <byohannes44@gmail.com>
2020-09-06 23:56:44 +0100

    moved server file to  the main directory

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const path = require("path");
+dotenv.config({ path: "./config/config.env" });
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1/classes", require("./routes/classes"));
+app.use("/api/v1/bookings", require("./routes/bookings"));
+app.use("/api/v1/courses", require("./routes/courseCalendar"));
+app.use("/api/v1/class/bookings", require("./routes/classBookings"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
