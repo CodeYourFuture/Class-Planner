f2fe4f1d0ed02fdfac3a66d1e959163a11076c10
AKaramifar <a.karamifar@gmail.com>
2020-09-11 23:33:34 +0100

    update upcomming class

--- frontend/src/screen/UpcomingClass/UpcomingClass.jsx
+++ frontend/src/screen/UpcomingClass/UpcomingClass.jsx
@@ -3,35 +3,60 @@ import Header from "../../components/Header/Header.jsx";
 import Loading from "../../components/Loading/Loading.jsx";
 import ClassCard from "../../components/ClassCard/ClassCard.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
-import { connect } from "react-redux";
-import { Get_UpcomingClass } from "../../redux/actions";
 import "./UpcomingClass.scss";
+import { useCallback } from "react";
+import axios from "axios";
+import dayjs from "dayjs";
+import { useState } from "react";
 
-const mapStateToProps = (state) => {
-  return {
-    Class: state.ClassReducer.Class,
-    pageData: state.PageReducer.pageData,
-  };
-};
+const UpcomingClass = ({ user, city, component }) => {
+  const [Class, setClass] = useState(null);
+  const [intakeName, setIntakeName] = useState(null);
+  const getUpcomingClass = useCallback(async () => {
+    let filtredCourses = null;
+    let courses = null;
+    let classFound = null;
+    await axios.get(`/api/v1/courses/`).then(async (response) => {
+      courses = response.data.data.filter((course) => course.cityName === city);
+      filtredCourses = courses.map((course) => course._id);
+      await axios.get(`/api/v1/classes`).then((res) => {
+        classFound = res.data.data
+          .sort((a, b) => (dayjs(a.date).isAfter(dayjs(b.date)) ? 1 : -1))
+          .find(
+            (Class) =>
+              filtredCourses.includes(Class.courseCalendar_Id) &&
+              Class.status &&
+              dayjs(Class.date) > dayjs(new Date())
+          );
+        setClass(classFound);
+        console.log(classFound);
+        setIntakeName(
+          courses.find((course) => course._id === classFound.courseCalendar_Id)
+            .intakeName
+        );
+      });
+    });
+  }, [city]);
 
-const UpcomingClass = ({ Class, Get_UpcomingClass, pageData }) => {
   useEffect(() => {
-    Get_UpcomingClass(pageData.city);
-  }, [Get_UpcomingClass, pageData.city]);
+    getUpcomingClass();
+  }, [getUpcomingClass]);
   return (
     <div>
-      <Header />
+      <Header user={user} city={city} component={component} />
       <div className="upcoming-class-container">
         <div className="upcoming-class-title">
-          <p>{pageData.city}</p> <i className="fas fa-chevron-right"></i>
-          <p>{pageData.title}</p>
+          <p>{city}</p> <i className="fas fa-chevron-right"></i>
+          <p>{intakeName}</p>
+          <i className="fas fa-chevron-right"></i>
+          <p>Upcoming Class</p>          
         </div>
         {Class ? (
           <ClassCard
-            Title={"Upcoming Class"}
-            Child={null}
+            user={user}
+            city={city}
+            component={component}
             Class={Class}
-            pageData={pageData}
           />
         ) : (
           <Loading />
@@ -41,4 +66,4 @@ const UpcomingClass = ({ Class, Get_UpcomingClass, pageData }) => {
     </div>
   );
 };
-export default connect(mapStateToProps, { Get_UpcomingClass })(UpcomingClass);
+export default UpcomingClass;
