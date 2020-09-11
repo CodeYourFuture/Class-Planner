c48b9b6f771a866d5bbb88c8d954fe4fb9f74fbd
AKaramifar <a.karamifar@gmail.com>
2020-09-11 23:33:16 +0100

    update newCoursePage

--- frontend/src/screen/NewCourse/NewCoursePage.jsx
+++ frontend/src/screen/NewCourse/NewCoursePage.jsx
@@ -2,23 +2,42 @@ import React from "react";
 import Header from "../../components/Header/Header.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
 import NewCourse from "../../components/NewCourse/NewCourse.jsx";
-import { connect } from "react-redux";
+import users from "../../data/users.json";
+import axios from "axios";
+import Loading from "../../components/Loading/Loading.jsx";
 import "./NewCoursePage.scss";
+import { useCallback } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
-const mapStateToProps = (state) => {
-  return { pageData: state.PageReducer.pageData };
-};
-
-const NewCoursePage = ({ pageData }) => {
+const NewCoursePage = ({ user, city, component }) => {
+  const [courses, setCourses] = useState(null);
+  const getCourses = useCallback(async () => {
+    await axios.get(`/api/v1/courses/`).then((response) => {
+      setCourses(response.data.data);
+    });
+  },[]);
+  useEffect(() => {
+    getCourses();
+  }, [getCourses]);
   return (
     <div>
-      <Header />
+      <Header user={user} city={city} component={component} />
       <div className="NewCourseCalendar">
-        {pageData.user === "admin" ? <NewCourse /> : null}
+        {user === users[0].id && courses ? (
+          <NewCourse
+            user={user}
+            city={city}
+            component={component}
+            courses={courses}
+          />
+        ) : (
+          <Loading />
+        )}
       </div>
       <Footer />
     </div>
   );
 };
 
-export default connect(mapStateToProps)(NewCoursePage);
+export default NewCoursePage;
