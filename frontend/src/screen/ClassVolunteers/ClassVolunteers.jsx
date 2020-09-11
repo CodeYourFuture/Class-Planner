5cce7f137405e343b0671bcb9854b0864cf71af2
AKaramifar <a.karamifar@gmail.com>
2020-09-11 23:30:41 +0100

    update class volunteers

--- frontend/src/screen/ClassVolunteers/ClassVolunteers.jsx
+++ frontend/src/screen/ClassVolunteers/ClassVolunteers.jsx
@@ -1,29 +1,42 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Header from "../../components/Header/Header.jsx";
 import ClassCard from "../../components/ClassCard/ClassCard.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
-import { connect } from "react-redux";
+import Loading from "../../components/Loading/Loading.jsx";
+import axios from "axios";
 
-const mapStateToProps = (state) => {
-  return {
-    pageData: state.PageReducer.pageData,
-  };
-};
-
-const ClassVolunteers = ({ pageData }) => {
+const ClassVolunteers = ({ user, city, component, id }) => {
+  const [Class, setClass] = useState(null);
+  const getClass = useCallback(async () => {
+    await axios.get(`/api/v1/classes/`).then((response) => {
+      setClass(response.data.data.find((_Class) => _Class._id === id));
+    });
+  }, [id]);
+  useEffect(() => {
+    getClass();
+  }, [getClass]);
   return (
     <div>
-      <Header />
+      <Header user={user} city={city} component={component} />
       <div className="upcoming-class-container">
-      <div className="upcoming-class-title">
-          <p>{pageData.city}</p> <i className="fas fa-chevron-right"></i>
-          <p>{pageData.title}</p>
+        <div className="upcoming-class-title">
+          <p>{city}</p> <i className="fas fa-chevron-right"></i>
+          <p>Atended Volunteers</p>
         </div>
-        <ClassCard />
+        {Class ? (
+          <ClassCard
+            user={user}
+            city={city}
+            component={component}
+            Class={Class}
+          />
+        ) : (
+          <Loading />
+        )}
       </div>
       <Footer />
     </div>
   );
 };
 
-export default connect(mapStateToProps)(ClassVolunteers);
+export default ClassVolunteers;
