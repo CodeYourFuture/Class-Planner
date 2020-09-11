cb9031b166d8cfb44465d7fc83e8bee6708f9c50
AKaramifar <a.karamifar@gmail.com>
2020-09-11 23:31:03 +0100

    update course calendar page

--- frontend/src/screen/CourseCalendar/CourseCalendarPage.jsx
+++ frontend/src/screen/CourseCalendar/CourseCalendarPage.jsx
@@ -3,11 +3,11 @@ import Header from "../../components/Header/Header.jsx";
 import CourseCalendar from "../../components/CourseCalendar/CourseCalendar.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
 
-const CourseCalendarPage = () => {
+const CourseCalendarPage = ({ user, city, component }) => {
   return (
     <div>
-      <Header />
-      <CourseCalendar />
+      <Header user={user} city={city} component={component} />
+      <CourseCalendar user={user} city={city} component={component} />
       <Footer />
     </div>
   );
