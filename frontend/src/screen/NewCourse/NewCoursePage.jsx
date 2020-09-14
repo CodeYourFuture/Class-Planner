4a3bdbc3d0427f053c73ed0a9c7c240f2a7233f5
AKaramifar <a.karamifar@gmail.com>
2020-09-14 15:39:10 +0100

    update newCoursePage

--- frontend/src/screen/NewCourse/NewCoursePage.jsx
+++ frontend/src/screen/NewCourse/NewCoursePage.jsx
@@ -1,35 +1,127 @@
 import React from "react";
+import { useForm } from "../../hooks/useForm.jsx";
 import Header from "../../components/Header/Header.jsx";
 import Footer from "../../components/Footer/Footer.jsx";
-import NewCourse from "../../components/NewCourse/NewCourse.jsx";
-import users from "../../data/users.json";
+import CourseForm from "../../components/CourseForm/CourseForm.jsx";
 import axios from "axios";
+import dayjs from "dayjs";
+import isBetween from "dayjs/plugin/isBetween";
 import Loading from "../../components/Loading/Loading.jsx";
 import "./NewCoursePage.scss";
 import { useCallback } from "react";
 import { useState } from "react";
 import { useEffect } from "react";
+import { useHistory } from "react-router";
 
 const NewCoursePage = ({ user, city, component }) => {
-  const [courses, setCourses] = useState(null);
-  const getCourses = useCallback(async () => {
-    await axios.get(`/api/v1/courses/`).then((response) => {
-      setCourses(response.data.data);
-    });
-  },[]);
+  const [citiesName, setCitiesName] = useState(null);
+  const [alertMessage, setAlertMessage] = useState(null);
+  const history = useHistory();
+  dayjs.extend(isBetween);
+  const getCourses = async () => {
+    const _Courses = await axios.get(`/api/v1/courses/`);
+    if (_Courses.data.data) {
+      return _Courses.data.data;
+    } else {
+      return false;
+    }
+  };
+  const getCitiesName = useCallback(async () => {
+    let _Courses = await getCourses();
+    if (_Courses) {
+      let _citiesName = _Courses.map((course) => course.cityName);
+      _citiesName = _citiesName.filter((a, b) => _citiesName.indexOf(a) === b);
+      setCitiesName(_citiesName);
+    } else {
+      setCitiesName("Nothing");
+    }
+  }, []);
+  const newCourseCalendar = async (values) => {
+    if (
+      dayjs(values.startDate).isAfter(dayjs(values.endDate)) ||
+      dayjs(values.startDate) === dayjs(values.endDate) ||
+      dayjs(values.startDate).diff(dayjs(values.endDate), "month", true) * -1 <
+        1
+    ) {
+      setAlertMessage({
+        type: "danger",
+        message:
+          "End Date must be after Start Date and the Course must be more than 1 month!",
+      });
+    } else {
+      let _Courses = await getCourses();
+      if (_Courses) {
+        _Courses = _Courses.find(
+          (course) =>
+            (dayjs(values.startDate).isBetween(
+              dayjs(course.startDate),
+              dayjs(course.endDate),
+              "day"
+            ) ||
+              dayjs(values.endDate).isBetween(
+                dayjs(course.startDate),
+                dayjs(course.endDate),
+                "day"
+              ) ||
+              course.intakeName === values.intakeName) &&
+            course.cityName === values.cityName
+        );
+      }
+
+      if (_Courses) {
+        setAlertMessage({
+          type: "danger",
+          message: "Your Data has conflict with other courses!",
+        });
+      } else {
+        await axios
+          .post(`/api/v1/courses`, {
+            ...values,
+          })
+          .then((response) => {
+            if (response.data.success) {
+              setAlertMessage({
+                type: "seccess",
+                message: "New Course Calendar added successfully !",
+              });
+              setTimeout(() => {
+                if (city) {
+                  history.push(`/${user}/${city}/courses/`);
+                } else {
+                  history.push(`/${user}/cities/`);
+                }
+              }, 2000);
+            } else {
+              setAlertMessage({
+                type: "danger",
+                message: "New Course Calendar was not added !",
+              });
+            }
+          });
+      }
+    }
+  };
+  const { entryData, error, onChange, onSubmit } = useForm(newCourseCalendar);
   useEffect(() => {
-    getCourses();
-  }, [getCourses]);
+    if (!city) {
+      getCitiesName();
+    }
+  }, [getCitiesName, city]);
+
   return (
     <div>
       <Header user={user} city={city} component={component} />
       <div className="NewCourseCalendar">
-        {user === users[0].id && courses ? (
-          <NewCourse
-            user={user}
+        {city || citiesName ? (
+          <CourseForm
             city={city}
             component={component}
-            courses={courses}
+            citiesName={citiesName}
+            alertMessage={alertMessage}
+            entryData={entryData}
+            error={error}
+            _onChange={onChange}
+            _onSubmit={onSubmit}
           />
         ) : (
           <Loading />
