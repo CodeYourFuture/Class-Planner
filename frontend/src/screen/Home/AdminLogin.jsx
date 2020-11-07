8aa0a7d6384b4f29660ecc0834da306a5e21c02f
byohannes <byohannes44@gmail.com>
2020-11-07 00:56:49 +0000

    updated admin login

--- frontend/src/screen/Home/AdminLogin.jsx
+++ frontend/src/screen/Home/AdminLogin.jsx
@@ -1,63 +1,30 @@
 import React, { useState } from "react";
-import axios from "axios";
-import { Redirect } from "react-router-dom";
 import users from "../../data/users.json";
 import "./AdminLogin.scss";
 
 const CancelBookingAlert = ({ closeHandler, showAlert }) => {
-  const [redirectToCities, setRedirectToCities] = useState(false);
   const [adminPassword, setAdminPassword] = useState("");
 
   const validateAdminPasswordInput = () => {
-    if (adminPassword !== "") {
-      validatePassword();
-      setRedirectToCities(true);
+    if (adminPassword === process.env.REACT_APP_Password) {
+      closeHandler();
+      showAlert("success", "The security code has been verified successfully.");
+      setTimeout(() => {
+        window.location.replace(`/${users[0].id}/cities`);
+      }, 500);
     } else {
-    //   closeHandler();
+      closeHandler();
       showAlert("danger", "Oops! The Security Code is incorrect.");
     }
   };
 
-  const validatePassword = () => {
-      const body={adminPassword};
-    axios
-      .put(`/api/v1/login/5fa5c5e5508ed1a9b56f5d54`,body )
-      .then((response) => {
-          console.log(response.data)
-        if (response.data.success === true) {
-        //   closeHandler();
-          showAlert(
-            "success",
-            "The security code has been verified successfully."
-          );
-         
-        }
-      })
-      .catch((err) => {
-          console.log(err)
-        // closeHandler();
-        if (err.response.data.success === false) {
-          showAlert("danger", err.response.data.error);
-        }
-      });
-  };
-
-  const redirectUser = () => {
-    if (redirectToCities) {
-      return <Redirect to={`/${users[0].id}/cities`} />;
-    }
-  };
   return (
     <React.Fragment>
       {
         <div className="password-alert">
-          <p>Please, Enter the Security Code ?</p>
+          <p>Please, Enter the Security Code?</p>
 
-          <form
-            onSubmit={() => {
-              validateAdminPasswordInput();
-            }}
-          >
+          <form onSubmit={validateAdminPasswordInput}>
             <div>
               <input
                 id="text"
@@ -71,7 +38,6 @@ const CancelBookingAlert = ({ closeHandler, showAlert }) => {
               </button>
             </div>
           </form>
-          {redirectUser()}
         </div>
       }
     </React.Fragment>
