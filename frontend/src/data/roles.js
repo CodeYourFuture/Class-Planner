d3879c88811c778861b1c8be153221f761d3715c
byohannes <byohannes44@gmail.com>
2020-12-14 14:25:59 +0000

    created roles file to include event and class volunteers role

---
+++ frontend/src/data/roles.js
@@ -0,0 +1,66 @@
+export const classRoles = [
+  {
+    id: 1,
+    roleName: "Coordinator",
+  },
+  {
+    id: 2,
+    roleName: "Lead Teacher",
+  },
+  {
+    id: 3,
+    roleName: "Assistant Lead Teacher",
+  },
+  {
+    id: 4,
+    roleName: "Teaching Assistant",
+  },
+  {
+    id: 5,
+    roleName: "Personal Development Rep",
+  },
+  {
+    id: 6,
+    roleName: "Zoom Master",
+  },
+];
+
+export const eventRoles = [
+  {
+    id: 1,
+    roleName: "Organizer",
+  },
+  {
+    id: 2,
+    roleName: "Presenter",
+  },
+  {
+    id: 3,
+    roleName: "Energizer",
+  },
+  {
+    id: 4,
+    roleName: "Zoom Master",
+  },
+  {
+    id: 5,
+    roleName: "Education Presenter",
+  },
+  {
+    id: 6,
+    roleName: "Personal Development Presenter",
+  },
+  {
+    id: 7,
+    roleName: "Ambassador Presenter",
+  },
+  {
+    id: 8,
+    roleName: "Graduate Story",
+  },
+];
+
+export default {
+  classRoles,
+  eventRoles,
+};
