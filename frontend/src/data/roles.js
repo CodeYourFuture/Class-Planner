c222c8b3759345d462bddf2c14a5b2b7b513b29d
byohannes <byohannes44@gmail.com>
2021-01-14 14:32:53 +0000

    updated roles for classes, weekly volunteer events and intro to coding event

--- frontend/src/data/roles.js
+++ frontend/src/data/roles.js
@@ -59,8 +59,46 @@ export const eventRoles = [
     roleName: "Graduate Story",
   },
 ];
+export const introRoles = [
+  {
+    id: 1,
+    roleName: "Zoom Master/ Lesson Lead (Experienced Volunteer)",
+  },
+  {
+    id: 2,
+    roleName: "Motivational Speaker/ Icebreaker (Student/Grad)",
+  },{
+    id: 3,
+    roleName: "Technical TA - INtro to CYF Presenter (Experienced Volunteer)",
+  },
+  {
+    id: 4,
+    roleName: "Technical TA - Sololearn (1)",
+  },
+  {
+    id: 5,
+    roleName: "Technical TA - Sololearn (2)",
+  },
+  {
+    id: 6,
+    roleName: "Technical TA - FreeCodeCamp Tutorial (1)",
+  },
+  {
+    id: 7,
+    roleName: "Technical TA - FreeCodeCamp Tutorial (2)",
+  },
+  {
+    id: 8,
+    roleName: "Technical TA - FreeCodeCamp Landing Page (1)",
+  },
+  {
+    id: 9,
+    roleName: "Technical TA - FreeCodeCamp Landing Page (2)",
+  }
+];
 
 export default {
   classRoles,
   eventRoles,
+  introRoles
 };
